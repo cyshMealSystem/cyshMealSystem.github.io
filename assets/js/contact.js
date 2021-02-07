129924653300c8f372ed05b8d0c346eaa28ecb20
Weng-john <59682713+Weng-john@users.noreply.github.com>
2021-02-07 19:42:19 +0800

    master

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,6 @@
-function submit(){
-    
+function send(){
+    var subject= document.getElementById("subject").value;
+    var body= document.getElementById("message").value;
+    document.getElementById("mailto").href ="mailto:text@gmail.com?" + "subject=" + subject + "&body=" + body;
+    document.getElementById("mailto").click();
 }
